870d33e3add8f63571591c92333b548d43cc62ed
gabriel-vieira12 <gabriel.vieira12@alunos.sc.senac.br>
2026-03-30 19:21:58 -0300

    contexto com autenticacao

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -1,51 +1,75 @@
-'use client';
+'use client'
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 
-import { createContext, ReactNode, useState, useContext } from "react";
-
-export class Usuario{
+export class Usuario {
     constructor(
-        public codigo: number,
+        public id: number|null,
         public nome: string,
-        public cpf: string,
-        public ativo: boolean
-    ){}
+        public email: string,
+        public status: string
+    ) { }
 }
 
-interface AuthContexType{
-    usuario:Usuario|null
-    token:string|null
-    login:(usuario: Usuario, token: string) => void,
-    logout:() => void
+interface AuthContextType {
+    usuario: Usuario | null,
+    token: string | null,
+    login: (usuario: Usuario, token: string) => void,
+    logout: () => void
 }
 
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-const AuthContext = createContext<AuthContexType|undefined>(undefined);
+export function AuthProvider({ children }: { children: ReactNode }) {
+    const [usuario, setUsuario] = useState<Usuario | null>(null);
+    const [token, setToken] = useState<string | null>(null);
+    const router = useRouter();
 
+    useEffect(() => {
+        debugger;
+        const usuarioRecover = Cookies.get('usuario');
+        const tokenRecover = Cookies.get('token');
 
-export function AuthProvider({ children }: {children:ReactNode}){
-    const [usuario, setUsuario] = useState<Usuario|null>(null);
-    const [token, setToken] = useState<string|null>(null);
-
-    const login = (usuario:Usuario, token:string) => {
-        setUsuario(usuario);
-        setToken(token);
-    }
-        const logout = () =>{
-            setUsuario(null);
-            setToken(null);   
+        if (usuarioRecover && tokenRecover) {
+            try {
+                setUsuario(JSON.parse(usuarioRecover));
+                setToken(tokenRecover);
+                
+                router.push(window.location.pathname)
+            } catch (e) {
+                console.error(e);
+            }
         }
 
-    return(
-        <AuthContext.Provider value={{usuario, token, login, logout}}>
+    }, []);
+
+    const login = (usuario: Usuario, token: string) => {
+        debugger;
+        setUsuario(usuario);
+        setToken(token);
+        Cookies.set('usuario', JSON.stringify(usuario), { expires: 7 });
+        Cookies.set('token', token, { expires: 7, secure: true })
+
+    }
+
+    const logout = () => {
+        setUsuario(null);
+        setToken(null);
+        Cookies.remove('usuario');
+        Cookies.remove('token');
+
+    }
+
+    return (
+        <AuthContext.Provider value={{ usuario, token, login, logout }}>
             {children}
         </AuthContext.Provider>
     )
-
 }
 
-export const useAuth = () =>{
+export const useAuth = () => {
     const context = useContext(AuthContext);
-    if(!context) throw new Error('useAuth deve ser usado dentro do provider')
-
-        return context;
+    if (!context) throw new Error('useAuth deve ser usado dentro do provider!')
+    return context;
 }
--- app/mock/usuario.tsx
+++ app/mock/usuario.tsx
@@ -1,42 +1,41 @@
 import { Usuario } from "@/app/context/AuthContext";
 
-
-
-export class UsuarioMock{
+export class UsuarioMock {
 
     private static usuarioDB: Usuario[] = [
-        new Usuario(1,"Professor Samuel Matos", "00000000000", true),
-        new Usuario(2,"Mateus", "00000000000", true),
-        new Usuario(3,"Carlos", "00000000000", true),
-        new Usuario(4,"Jose", "00000000000", true),
-        new Usuario(5,"Paulo", "00000000000", true)
+        new Usuario(1, "Professor Samuel Matos", "00000000000", true),
+        new Usuario(2, "Matheus", "00000000000", true),
+        new Usuario(3, "Carlos", "00000000000", true),
+        new Usuario(4, "Jose", "00000000000", true),
+        new Usuario(5, "Paulo", "00000000000", true)
     ];
 
-    static async listarTodos(): Promise<Usuario[]>{
+    static async listarTodos(): Promise<Usuario[]> {
         return [...this.usuarioDB]
     }
 
-    static async salvar(usuario:Usuario) : Promise<void>{
+    static async salvar(usuario: Usuario): Promise<void> {
 
-        const indexExistente = this.usuarioDB.findIndex(u=> u.codigo === usuario.codigo);
 
-        const novoCodigo = Math.max(...this.usuarioDB.map(u=>u.codigo))+1;
+        const indexExistente = this.usuarioDB.findIndex(u => u.codigo === usuario.codigo);
 
-        if(indexExistente=== -1){
+        if (indexExistente === -1) {
 
+            const novoCodigo = Math.max(...this.usuarioDB.map(u => u.codigo)) + 1;
             usuario.codigo = novoCodigo;
             this.usuarioDB.push(usuario);
-            console.log(`Usuário de 10 ${novoCodigo} salvo com sucesso!`);
-        
+            console.log(`Usuario de ID ${novoCodigo} salvo com sucesso!`);
+
         }else{
             this.usuarioDB[indexExistente].nome = usuario.nome;
             this.usuarioDB[indexExistente].cpf = usuario.cpf;
-            console.log(`Usuário de 10 ${usuario.codigo} atualizado com sucesso!`);
+
+            console.log(`Usuario de ID ${usuario.codigo} atualizado com sucesso!`);
         }
     }
 
-    static async buscarPorId(codigo:Number):Promise<Usuario|undefined>{
-        return this.usuarioDB.find(u=>u.codigo === codigo)
+    static async buscarPorId(codigo: Number): Promise<Usuario | undefined> {
+
+        return this.usuarioDB.find(u => u.codigo === codigo);
     }
-        
 }
