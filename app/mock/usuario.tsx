565eb600fe56fa2dff2640c3bf4f8304d4acc221
Gabriel Vieira <gabrielsy.cacamba@gmail.com>
2026-04-07 11:42:22 -0300

    login sindicos funcional

--- app/mock/usuario.tsx
+++ app/mock/usuario.tsx
@@ -2,15 +2,16 @@ import { Usuario } from "@/app/context/AuthContext";
 
 export class UsuarioMock {
     private static usuarioDB: Usuario[] = [
-        new Usuario(1, "Gabriel Vieira", "gabriel@email.com", "ATIVO", "Condomínio Central", "123456"),
-        new Usuario(2, "Matheus", "matheus@email.com", "ATIVO", "Residencial Sol", "123456"),
-        new Usuario(3, "Carlos", "carlos@email.com", "ATIVO", "Jardim das Flores", "123456"),
-        new Usuario(4, "José", "jose@email.com", "INATIVO", "Condomínio Azul", "123456"),
-        new Usuario(5, "Paulo", "paulo@email.com", "ATIVO", "Residencial São Pedro", "123456")
+        new Usuario(1, "Gabriel Vieira", "gabriel@email.com", "ATIVO", "Residencial Solar das Palmeiras", "123456"),
+        new Usuario(2, "Samuel Matos", "samuel@email.com", "ATIVO", "Condomínio Central", "123456")
     ];
 
     static async listarTodos(): Promise<Usuario[]> {
         return [...this.usuarioDB];
+    }
+
+    static async buscarPorId(id: number): Promise<Usuario | undefined> {
+        return this.usuarioDB.find(u => u.id === id);
     }
 
     static async salvar(usuario: Usuario): Promise<void> {
@@ -24,14 +25,16 @@ export class UsuarioMock {
 
             usuario.id = novoId;
             this.usuarioDB.push(usuario);
-            console.log(`Usuário de ID ${novoId} salvo com sucesso!`);
         } else {
             this.usuarioDB[indexExistente] = usuario;
-            console.log(`Usuário de ID ${usuario.id} atualizado com sucesso!`);
         }
     }
 
-    static async buscarPorId(id: number): Promise<Usuario | undefined> {
-        return this.usuarioDB.find(u => u.id === id);
+    static async autenticar(email: string, senha: string): Promise<Usuario | null> {
+        const usuario = this.usuarioDB.find(
+            u => u.email === email && u.senha === senha
+        );
+
+        return usuario || null;
     }
 }
