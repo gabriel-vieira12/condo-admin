adad185f5b9639ab1e9d28901d6de55475f5c632
Gabriel Vieira <gabrielsy.cacamba@gmail.com>
2026-06-19 08:57:28 -0300

    ajustando dto requests v2

--- backend/src/main/java/com/senac/condo_admin/domain/entities/Usuario.java
+++ backend/src/main/java/com/senac/condo_admin/domain/entities/Usuario.java
@@ -2,6 +2,7 @@ package com.senac.condo_admin.domain.entities;
 
 
 import com.senac.condo_admin.application.DTO.UsuarioAdmRequest;
+import com.senac.condo_admin.application.DTO.UsuarioRequest;
 import com.senac.condo_admin.domain.enuns.EnumStatusUsuario;
 import com.senac.condo_admin.domain.valueobjects.CPF;
 import jakarta.persistence.*;
--- backend/src/main/java/com/senac/condo_admin/presentation/controller/UsuarioController.java
+++ backend/src/main/java/com/senac/condo_admin/presentation/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.senac.condo_admin.presentation.controller;
 
 import com.senac.condo_admin.application.DTO.AlterarStatusRequest;
 import com.senac.condo_admin.application.DTO.UsuarioAdmRequest;
+import com.senac.condo_admin.application.DTO.UsuarioRequest;
 import com.senac.condo_admin.application.DTO.UsuarioResponse;
 import com.senac.condo_admin.application.services.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
